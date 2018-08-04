f495e0f0298b32a00fdf64402afdd28bcaad1c2e
Matias <mpierdominici@itba.edu.ar>
2018-08-04 16:10:38 -0300

    r

---
+++ ej 3/comercialValuesGenerator.m
@@ -0,0 +1,11 @@
+function [ comercialValues ] = comercialValuesGenerator( tolerancia )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+ switch tolerancia
+     case 10
+     case 5
+     case 2
+     case 1
+
+end
+
