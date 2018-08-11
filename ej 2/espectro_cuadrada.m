a22e4dc34c7548241112c8fcd155f1b8b4417691
rocioparra <rparra@itba.edu.ar>
2018-08-11 16:13:05 -0300

    graficos matlab+latex ejercicio 2

--- ej 2/espectro_cuadrada.m
+++ ej 2/espectro_cuadrada.m
@@ -10,7 +10,7 @@ y = zeros([1 N]);
 
 for n = 1:N  
      if(rem(n,2))   %si es impar
-         y(n) = 4 / n;
+         y(n) = 4 / n / pi();
      end;
 end;
 
@@ -23,3 +23,11 @@ set(gca,'color','none')
 scatter(x,y,'*');
 title('Diagrama espectral de señal de entrada')
 grid on;
+
+
+xticks(f:2*f:19*f);
+set(gca,'TickLabelInterpreter','latex');
+xticklabels({'$f$', '$3f$', '$5f$', '$7f$', '$9f$', '$11f$', '$13f$', '$15f$', '$17f$', '$19f$'});
+xlabel('Frecuencia','interpreter','latex');
+ylabel('Amplitud(V)','interpreter','latex');
+title('Diagrama espectral de se\~nal de entrada', 'interpreter', 'latex');
