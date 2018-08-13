5fdcedd01f1bd0e058508dcc9a44b2b30346367c
malustewart <mastewart@itba.edu.ar>
2018-08-12 23:55:40 -0300

    avance informe ej 2

--- ej 2/generarBodeConCsv.m
+++ ej 2/generarBodeConCsv.m
@@ -4,6 +4,19 @@ datos=csvread('bode.csv');
 ganancia=datos(:,3)./datos(:,2);
 ganancia=20*log10(ganancia);
 fase=datos(:,4);
-frecuencia=datos(:,1).*1000;
-%semilogx(frecuencia,ganancia)
+frecuencia=datos(:,1);
+
+figure;
+
+subplot(2,1,1);
+semilogx(frecuencia,ganancia)
+hold on;
+grid on;
+xlabel('Frecuencia (KHz)');
+ylabel('Ganancia (dB)');
+
+subplot(2,1,2);
 semilogx(frecuencia,fase)
+grid on;
+xlabel('Frecuencia (KHz)');
+ylabel('Fase');
